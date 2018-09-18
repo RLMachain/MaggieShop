4b7aecd2f6f59d9895d89c8fd4e6d87774406dcb
RLMachain <rolopezma@ittepic.edu.mx>
2018-09-18 01:35:29 -0500

    layout login

--- app/src/main/java/machain/com/maggieshop/Login.java
+++ app/src/main/java/machain/com/maggieshop/Login.java
@@ -1,12 +1,21 @@
 package machain.com.maggieshop;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
-public class Login extends AppCompatActivity {
+public class Login extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.layout_login);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.layout_login);
+
+
     }
 }
